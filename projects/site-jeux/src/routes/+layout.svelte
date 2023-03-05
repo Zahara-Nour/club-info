efb22ed6fad5d74163c6437e864b93f2aeb1c059
David Le Jolly <zahara.alnour@gmail.com>
2023-03-05 07:15:53 +0300

    Tic Tac Toe

--- projects/site-jeux/src/routes/+layout.svelte
+++ projects/site-jeux/src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	import '@skeletonlabs/skeleton/styles/all.css';
-	import '../app.postcss';
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css'
+	import '@skeletonlabs/skeleton/styles/all.css'
+	import '../app.postcss'
 </script>
+
+<ul class="flex gap-4">
+	<li><a href="/">Home</a></li>
+	<li><a href="/jeux">Jeux</a></li>
+</ul>
 <slot />
---
+++ projects/site-jeux/src/routes/jeux/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="my-4">Les jeux</h1>
+
+<a href="/jeux/TicTacToe">Tic Tac Toe</a>
