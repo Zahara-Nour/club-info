ec9605e805a04ab5ad73e8181d54024e73b987f1
David Le Jolly <zahara.alnour@gmail.com>
2024-01-10 18:30:06 +0300

    add skeleton ui

--- projects/2024/src/routes/+page.svelte
+++ projects/2024/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="container mx-auto space-y-8 p-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+</div>
+
+<Avatar src="https://i.pravatar.cc/" />
