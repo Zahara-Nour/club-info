bae501a675721d9ab94d03e9ca1016fb6c385772
David Le Jolly <zahara.alnour@gmail.com>
2024-01-10 20:32:11 +0300

    add skeleton ui

--- projects/2024/src/routes/+layout.svelte
+++ projects/2024/src/routes/+layout.svelte
@@ -1,18 +1,50 @@
-<script>
-	import { AppShell } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import {
+		AppShell,
+		Drawer,
+		Modal,
+		Toast,
+		getDrawerStore,
+		initializeStores,
+		storePopup
+	} from '@skeletonlabs/skeleton';
 	import '../app.pcss';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import Navigation from '$lib/ui/Navigation.svelte';
+	import TopBar from '$lib/ui/TopBar.svelte';
+
+	function openDrawer(): void {
+		drawer.open({});
+	}
+
+	function closeDrawer(): void {
+		drawer.close();
+	}
+
+	// modals, popups and drawer initialization
+	initializeStores();
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+	const drawer = getDrawerStore();
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-	<svelte:fragment slot="sidebarLeft">
-		<!-- Insert the list: -->
-		<nav class="list-nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-			</ul>
-		</nav>
-		<!-- --- -->
+<Drawer><Navigation drawerClose={closeDrawer} /></Drawer>
+
+<AppShell slotSidebarLeft="bg-surface-100-800-token" regionPage="relative">
+	<svelte:fragment slot="header">
+		<TopBar drawerOpen={openDrawer} />
 	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<div id="sidebar-left" class="hidden lg:block lg:w-60">
+			<Navigation />
+		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">
+		<div id="sidebar-left" class="hidden lg:block" />
+	</svelte:fragment>
+
+	<!-- Router Slot -->
+
 	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter" />
 </AppShell>
