3a4d330a2428bd70ebc739a97ef5d3840f9d8259
David Le Jolly <zahara.alnour@gmail.com>
2024-01-10 19:49:32 +0300

    add skeleton ui

--- projects/2024/src/routes/+layout.svelte
+++ projects/2024/src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
+	import { AppShell } from '@skeletonlabs/skeleton';
 	import '../app.pcss';
 </script>
 
-<slot />
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Insert the list: -->
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</nav>
+		<!-- --- -->
+	</svelte:fragment>
+	<slot />
+</AppShell>
